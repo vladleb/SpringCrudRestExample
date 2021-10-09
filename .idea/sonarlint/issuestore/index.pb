20dd9fd34fb2144c6f92c0f95c733092ef6311bc
Lebedev Vladislav <vladis90@bk.ru>
2021-10-09 12:20:06 +0300

    Ready project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+‹
+[src/main/java/ru/lebedev/springCrudRest/config/MySpringMvcDispatcherServletInitializer.java,f\1\f153fb785dd76bd911fb6fe16c9cafe4ea37e36c
+_
+/src/main/webapp/WEB-INF/views/people/index.html,5\9\591951c680a02b468822ccbb4e524d1538e14bac
+^
+.src/main/webapp/WEB-INF/views/people/show.html,0\0\00b704871c09fc6987aa529a2b388d65baa7a12f
+]
+-src/main/webapp/WEB-INF/views/people/new.html,9\7\97c3b486fa9c121f92d9aff1f5f3e8b144c7451f
+^
+.src/main/webapp/WEB-INF/views/people/edit.html,6\5\65684609741f2fbbbec8a75635079c03817e4212
---
+++ src/main/java/ru/lebedev/springCrudRest/dao/PersonMapper.java
@@ -0,0 +1,21 @@
+package ru.lebedev.springCrudRest.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.lebedev.springCrudRest.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+}
